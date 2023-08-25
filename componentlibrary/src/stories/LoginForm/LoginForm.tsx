27455a8df7eca6fce09f79770183198b6837cfcf
Per Hartveit <per.hartveit@knowit.no>
2023-08-25 15:53:54 +0200

    storybook component library: beginnings with loginForm

---
+++ componentlibrary/src/stories/LoginForm/LoginForm.tsx
@@ -0,0 +1,36 @@
+import "./LoginForm.css";
+
+export interface ILoginForm {
+  usernamePlaceholder: string;
+  passwordPlaceholder: string;
+}
+export const LoginForm = (props: ILoginForm) => {
+  return (
+    <div id="logincontainer">
+      <div>Logg inn :)</div>
+      <form id="loginform">
+        <div id="username">
+          <label>Username</label>
+          <input
+            type="email"
+            name=""
+            id=""
+            placeholder={props.usernamePlaceholder}
+          />
+        </div>
+        <div id="password">
+          <label>Password</label>
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder={props.passwordPlaceholder}
+          />
+        </div>
+        <div id="forgottenPassword">
+          <a href="">Forgotten password?</a>
+        </div>
+      </form>
+    </div>
+  );
+};
